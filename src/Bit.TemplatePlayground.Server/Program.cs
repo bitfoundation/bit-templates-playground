cf93b60fbd8e2c2547ec06521928be4f3ffa3ee6
Yaser Moradi <ysmoradi@outlook.com>
2023-12-20 23:04:57 +0100

    feat: update to bit 8.5.0 #35 (#36)

--- src/Bit.TemplatePlayground.Server/Program.cs
+++ src/Bit.TemplatePlayground.Server/Program.cs
@@ -5,7 +5,7 @@ builder.Configuration.AddClientConfigurations();
 // The following line (using the * in the URL), allows the emulators and mobile devices to access the app using the host IP address.
 if (BuildConfiguration.IsDebug() && OperatingSystem.IsWindows())
 {
-    builder.WebHost.UseUrls("https://localhost:5031", "http://localhost:5030", "https://*:5031", "http://*:5030");
+    builder.WebHost.UseUrls("http://localhost:5030", "http://*:5030");
 }
 
 Bit.TemplatePlayground.Server.Startup.Services.Add(builder.Services, builder.Environment, builder.Configuration);
