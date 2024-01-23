ebc571f7d0e5c99b89fb960a96f03b00e9a51f30
Yaser Moradi <ysmoradi@outlook.com>
2024-01-23 18:23:48 +0100

    feat: update to bit 8.7.4 #43 (#44)

---
+++ src/Client/Bit.TemplatePlayground.Client.Windows/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Http;
+
+namespace Bit.TemplatePlayground.Client.Windows;
+
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        AppRenderMode.IsBlazorHybrid = true;
+        var services = new ServiceCollection();
+        ConfigurationBuilder configurationBuilder = new();
+        configurationBuilder.AddClientConfigurations();
+        var configuration = configurationBuilder.Build();
+        services.AddTransient<IConfiguration>(sp => configuration);
+        Uri.TryCreate(configuration.GetApiServerAddress(), UriKind.Absolute, out var apiServerAddress);
+        services.AddTransient(sp =>
+        {
+            var handler = sp.GetRequiredKeyedService<HttpMessageHandler>("DefaultMessageHandler");
+            HttpClient httpClient = new(handler)
+            {
+                BaseAddress = apiServerAddress
+            };
+            return httpClient;
+        });
+        services.AddWpfBlazorWebView();
+        if (BuildConfiguration.IsDebug())
+        {
+            services.AddBlazorWebViewDeveloperTools();
+        }
+        services.AddWindowsServices();
+        InitializeComponent();
+        BlazorWebView.Services = services.BuildServiceProvider();
+        BlazorWebView.Services.GetRequiredService<CultureInfoManager>().SetCurrentCulture(App.Current.Properties["Culture"]?.ToString());
+        BlazorWebView.Services.GetRequiredService<IPubSubService>().Subscribe(PubSubMessages.CULTURE_CHANGED, async culture =>
+        {
+            App.Current.Shutdown();
+            Application.Restart();
+        });
+        BlazorWebView.Loaded += async delegate
+        {
+            await BlazorWebView.WebView.EnsureCoreWebView2Async();
+
+            BlazorWebView.WebView.NavigationCompleted += async delegate
+            {
+                await BlazorWebView.WebView.ExecuteScriptAsync("Blazor.start()");
+            };
+        };
+    }
+}
