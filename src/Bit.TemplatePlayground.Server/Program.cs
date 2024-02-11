30f8987b28341004877db31e0b299bdf9ca8cd44
Yaser Moradi <ysmoradi@outlook.com>
2024-02-11 21:39:18 +0100

    Update to bit 8.7.5

--- src/Bit.TemplatePlayground.Server/Program.cs
+++ src/Bit.TemplatePlayground.Server/Program.cs
@@ -1,17 +1,26 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿
+namespace Bit.TemplatePlayground.Server;
 
-builder.Configuration.AddClientConfigurations();
-
-// The following line (using the * in the URL), allows the emulators and mobile devices to access the app using the host IP address.
-if (BuildConfiguration.IsDebug() && OperatingSystem.IsWindows())
+public static partial class Program
 {
-    builder.WebHost.UseUrls("http://localhost:5030", "http://*:5030");
+    public static async Task Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Configuration.AddClientConfigurations();
+
+        // The following line (using the * in the URL), allows the emulators and mobile devices to access the app using the host IP address.
+        if (BuildConfiguration.IsDebug() && OperatingSystem.IsWindows())
+        {
+            builder.WebHost.UseUrls("http://localhost:5030", "http://*:5030");
+        }
+
+        builder.ConfigureServices();
+
+        var app = builder.Build();
+
+        app.ConfiureMiddlewares();
+
+        await app.RunAsync();
+    }
 }
-
-Bit.TemplatePlayground.Server.Startup.Services.Add(builder.Services, builder.Environment, builder.Configuration);
-
-var app = builder.Build();
-
-Bit.TemplatePlayground.Server.Startup.Middlewares.Use(app, builder.Environment, builder.Configuration);
-
-await app.RunAsync();
